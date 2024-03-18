63e9495bf058c2b37a1009b6fe3180494a876331
noyononline <noyononlinen@gmail.com>
2024-03-18 14:50:19 +0600

    fibonaci number

--- Practices-2/src/app.js
+++ Practices-2/src/app.js
@@ -1,7 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/api/calculate", (req, res) => {
+  try {
+    const value = parseInt(req.query.value);
+    res.json({ originalValue: value, resust: fibonacci(value) });
+  } catch (error) {
+    res.json({ msg: "Invalid query value", value: req.query.value });
+  }
+});
+app.listen(4000, () => {
+  console.log("server is Lisitening on port 4000");
+});
+
 function fibonacci(num) {
   if (num <= 1) return 1;
 
   return fibonacci(num - 1) + fibonacci(num - 2);
 }
-
-console.log(fibonacci(100));
